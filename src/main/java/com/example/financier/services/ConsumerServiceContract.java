2a52fc8aaa539c2e1d7111f4268a92da6559c237
Elbek-ibn-Muzaffar <81406357+Elbek-ibn-Muzaffar@users.noreply.github.com>
2021-09-08 09:22:54 +0500

    Update ConsumerServiceContract.java

--- src/main/java/com/example/financier/services/ConsumerServiceContract.java
+++ src/main/java/com/example/financier/services/ConsumerServiceContract.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient("http://Consumer-app")
+@FeignClient("https://Consumer-app")
 public interface ConsumerServiceContract {
 
     @GetMapping("/api/v2/consumerApp/getAllCosts")
